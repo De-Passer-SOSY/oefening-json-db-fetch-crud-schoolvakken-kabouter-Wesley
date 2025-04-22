451cc022c5ee98e91f7983a1f6b1592a31718817
kabouter-Wesley <simon.depoorter@leerling.de-passer.be>
2025-04-22 10:51:42 +0200

    add create task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,27 @@ async function fetchDb() {
         const response = await fetch("http://localhost:5688/vakken");
         return await response.json();
     } catch (err) {
-        console.error(`Er is een fout opgetreden ${err}`);
+        console.error(`Er is een fout opgetreden: ${err}`);
+    }
+}
+
+async function createTask() {
+    const input = document.querySelector("#vakInput");
+    const content = input.value.trim();
+
+    try {
+        const response = await fetch("http://localhost:5688/vakken", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({name: content}),
+        });
+        if (response.ok) {
+            input.value = "";
+            fetchDb();
+        }
+    } catch (err) {
+        console.error(`Er is een fout opgetreden: ${err}`);
     }
 }
